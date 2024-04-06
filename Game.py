22721826567b5680491c2ad2947f925e790dac2a
Alexander Winberg <alexander.winberg.036@gmail.com>
2024-04-06 23:02:31 +0200

    Menu added

--- Game.py
+++ Game.py
@@ -1,4 +1,6 @@
+import sys
 import pygame
+from Menu import Menu
 
 CELL_SIZE = 15  # Storleken på varje cell i pixlar
 GRID_SIZE = 32  # Antal celler i varje riktning
@@ -10,6 +12,7 @@ class Game: # Klassen som hanterar spelet
     def __init__(self): 
         self.screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE)) # Skapar ett fönster
         self.running = True # Variabel som håller koll på om spelet körs eller inte
+        self.menu = Menu(self.screen) # Skapar Meny-objektet
 
 
             
@@ -19,23 +22,34 @@ class Game: # Klassen som hanterar spelet
                 rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
                 pygame.draw.rect(self.screen, (128, 128, 128), rect, 1)
                 
-                if x == 0 or y == 0 or x == SCREEN_SIZE - CELL_SIZE or y == SCREEN_SIZE - CELL_SIZE:
-                    inner_rect = pygame.Rect(x + 1, y + 1, CELL_SIZE - 2, CELL_SIZE - 2)
-                    pygame.draw.rect(self.screen, (255, 255, 255), inner_rect, 0)  # Fill the inner rectangle
-                else:
-                    inner_rect = pygame.Rect(x + 1, y + 1, CELL_SIZE - 2, CELL_SIZE - 2)
+                if x == 0 or y == 0 or x == SCREEN_SIZE - CELL_SIZE or y == SCREEN_SIZE - CELL_SIZE: # Om det är en kantcell
+                    inner_rect = pygame.Rect(x + 1, y + 1, CELL_SIZE - 2, CELL_SIZE - 2) 
+                    pygame.draw.rect(self.screen, (255, 255, 255), inner_rect, 0)  
+                else: # Om det inte är en kantcell
+                    inner_rect = pygame.Rect(x + 1, y + 1, CELL_SIZE - 2, CELL_SIZE - 2) 
                     pygame.draw.rect(self.screen, (47, 79, 79), inner_rect, 0)
         
     def run(self): # Huvudloopen för spelet
-        while self.running: 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+        choice = None
+        while choice is None:
+            self.menu.display()
+            pygame.display.flip()
+            choice = self.menu.handle_input()
 
-            self.draw_grid()   
-            pygame.display.flip() # Uppdaterar fönstret
-        
-        pygame.quit() # Stänger ner pygame
+        print("choice ", str(choice))
+
+        if choice == "Exit":
+            pygame.quit()
+        else:
+            while self.running: 
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+
+                self.draw_grid()   
+                pygame.display.flip()  # Uppdaterar fönstret
+
+        pygame.quit()  # Stänger ner pygame
         
         
 def main():
---
+++ Menu.py
@@ -0,0 +1,48 @@
+import pygame
+pygame.init() 
+
+class Menu:
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pygame.font.Font(None, 36)
+        self.menu_options = ["Start", "Borders", "Dificulty", "Highscore", "Exit"] # Menyvalen
+
+    def display(self): # Funktion som ritar ut menyn
+        self.menu_items = []
+        title_font = pygame.font.Font(None, 62) # Skapar en font för titeln
+        title = title_font.render("SNAKE v_0.0.1", True, (220, 20, 60)) # Skapar titeln
+        title_height = title.get_height() 
+        option_height = self.font.render(self.menu_options[0], True, (0, 0, 0)).get_height() # Höjden på varje menyval
+        spacing = 20  # Avstånd mellan varje menyval
+        total_height = title_height + len(self.menu_options) * (option_height + spacing)
+
+        title_y = (self.screen.get_height() - total_height) // 2 
+        title_rect = title.get_rect(center=(self.screen.get_width() // 2, title_y)) # Centrerar titeln
+        self.screen.blit(title, title_rect)
+
+        mouse_pos = pygame.mouse.get_pos()  # Hämtar musens position
+
+        for i, option in enumerate(self.menu_options): # Loopar igenom varje menyval for att hitta vilken som ska markeras
+            text_y = title_y + title_height + i * (option_height + spacing) + spacing   
+            text_rect = pygame.Rect(self.screen.get_width() // 2, text_y, option_height, option_height)
+
+            if text_rect.collidepoint(mouse_pos): 
+                color = (225, 225, 222)  
+            else:
+                color = (75, 75, 75) 
+
+            text_rendered = self.font.render(option, True, color)  # Skapar texten för varje menyval
+            text_rect = text_rendered.get_rect(center=(self.screen.get_width() // 2, text_y))
+            self.screen.blit(text_rendered, text_rect)
+            self.menu_items.append((option, text_rendered, text_rect))
+            
+            
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                for option, _, rect in self.menu_items:
+                    if rect.collidepoint(mouse_pos):
+                        return option
+        return None
+            
